ab122769d9868a8955fbcd4df0356f645847a49e
Tom Bombadil <amanibhavam@destructuring.org>
2012-10-08 21:12:49 -0700

    install linux-virtual kernel instead of -server

--- ec2.sh
+++ ec2.sh
@@ -7,8 +7,8 @@ export DEBIAN_FRONTEND="noninteractive"
 
 # update packages
 aptitude update
-aptitude install -q -y linux-server linux-headers-server linux-image-server linux-headers
+aptitude install -q -y linux-virtual linux-headers-virtual linux-image-virtual linux-headers
 aptitude install -q -y ntp
-aptitude hold linux-server linux-headers-server linux-image-server linux-headers
+aptitude hold linux-virtual linux-headers-virtual linux-image-virtual linux-headers
 aptitude safe-upgrade -q -y
 aptitude clean
